db3bff46ec291d034d06a73184804f9fd197c2d3
shoaibmerchant89 <shoaibmerchant89@gmail.com>
2020-04-04 18:43:49 -0400

    first commit

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntpd' do
+  action :install
+  ignore_failure true
+end
+
+file '/etc/motd' do
+  content 'This server is the property of Shoaib'  
+  owner 'root'
+  group 'root'
+  action :create
+end
